9a489940c11b97a3b7e0aa6a791f9b77787ee66a
AaronL87 <49658070+AaronL87@users.noreply.github.com>
2020-06-15 23:42:48 -0400

    Update link_nodes.py

--- link_nodes.py
+++ link_nodes.py
@@ -21,3 +21,21 @@ class LinkNodes: # Left and right are from perspective facing convex hull from o
     @staticmethod
     def Midpoint(point1,point2): # Midpoint Formula
         return ((point1[0]+point2[0])/2,(point1[1]+point2[1])/2)
+
+class AddNode:
+    def __init__(self,point,left,right,midDict):
+        self.point = point
+        self.left = left # Clockwise
+        self.right = right # Counterclockwise
+        self.leftMidpoint = self.Midpoint(self.point,self.left)
+        self.rightMidpoint = self.Midpoint(self.point,self.right)
+
+        # update midpointDict
+        midDict[self.leftMidpoint] = [left,point]
+        midDict[self.rightMidpoint] = [point,right]
+        
+        print("midpointDict:",midDict,"\n"*2)
+
+    @staticmethod
+    def Midpoint(point1,point2): # Midpoint Formula
+        return ((point1[0]+point2[0])/2,(point1[1]+point2[1])/2)
