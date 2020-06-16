d9b9ed9be09b7d4ce9156b3850f7763f343c98d1
AaronL87 <49658070+AaronL87@users.noreply.github.com>
2020-06-15 22:51:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ class TravelingSalesmanMidpointAlgo:
         begin = time.time()
         while len(self.convexHull.IP) != 0:
             self.GetMinOR()
-            self.UpdateAll()
+            self.Update_OP_IP()
+            self.UpdateAllDicts()
         end = time.time()
         print("Time:",end-begin,"\n"*2)
 
@@ -26,7 +27,7 @@ class TravelingSalesmanMidpointAlgo:
         return ((point1[0]-point2[0])**2+(point1[1]-point2[1])**2)**.5 # Taking sqrt not needed
 
     @staticmethod
-    def GetAngle(leftOP,rightOP,IP): # Find angle to order IP when one MP connects to multiple IP
+    def GetAngle(leftOP,rightOP,IP): # Find angle to order IP when one MP connects to multiple IP (Angle is from right OP's perspective)
         vector_1 = (leftOP[0]-rightOP[0],leftOP[1]-rightOP[1])
         vector_2 = (IP[0]-rightOP[0],IP[1]-rightOP[1])
 
@@ -34,7 +35,7 @@ class TravelingSalesmanMidpointAlgo:
         unitVector_2 = vector_2 / np.linalg.norm(vector_2)
         dot_product = np.dot(unitVector_1, unitVector_2)
         angle = np.arccos(dot_product)
-        return angle #TODO fix return
+        return angle
     
     def GetMidpointToIPs(self): # Gets all distances from each Midpoint to IP
         print("midpointsToIPs:","\n")
@@ -56,7 +57,7 @@ class TravelingSalesmanMidpointAlgo:
             tempDict.update({b:tempVal}) 
             tempList.append((b,tempVal)) # TODO May not need list because it won't be sorted
         
-        tempList.sort(key=lambda tup: tup[1]) # TODO Don't sort. Find smallest instead, and put it in the front of every list whenever you pull out smallest. Keep track of whether smallest is deleted. If so, find smallest for that list. Put min at front of list (will require marker to know it is min).
+        tempList.sort(key=lambda tup: tup[1]) # TODO Don't sort. Always put min(s) at the front of lists
         if directionString.lower() == "right":    
             print(OP1.rightMidpoint,":",tempList,"\n")
             self.midpointsToIPsRef.update({OP1.rightMidpoint:tempDict}) # Used as reference to delete from midpointsToIPs
@@ -68,7 +69,9 @@ class TravelingSalesmanMidpointAlgo:
 
     def GetMinOR(self): # Gets smallest outer radius (from OP to IP)
         minVal = float("inf")
+        self.MidToIPDict = {}
         self.IPToMidDict = {}
+        self.SetIP = set()
         for k,v in self.midpointsToIPs.items():
             if v[0][1] == minVal:
                 self.CheckForMultiOR(k,v,minVal)
@@ -76,6 +79,7 @@ class TravelingSalesmanMidpointAlgo:
                 minVal = v[0][1]
                 self.MidToIPDict = {}
                 self.IPToMidDict = {}
+                self.SetIP = set()
                 self.CheckForMultiOR(k,v,minVal)
         
         print("MidToIPDict:",self.MidToIPDict,"\n")
@@ -84,80 +88,77 @@ class TravelingSalesmanMidpointAlgo:
     def CheckForMultiOR(self,key,val,minVal): # Checks if an OP connects to multiple IP
         self.UpdateMidToIPDict(key,val[0][0],val[0][1])
         self.UpdateIPToMidDict(val[0][0],key,val[0][1])
+        self.SetIP.add(val[0][0])
         for i in range(len(val)-1):
             if val[i+1][1] == minVal:
-                self.UpdateMidToIPDict(key,val[i+1][0],val[i+1][1]) # Not currently used
+                self.UpdateMidToIPDict(key,val[i+1][0],val[i+1][1])
                 self.UpdateIPToMidDict(val[i+1][0],key,val[i+1][1])
+                self.SetIP.add(val[i+1][0])
             else:
                 break
 
-    def UpdateMidToIPDict(self,MP,IP,Dist): # Not currently used
+    def UpdateMidToIPDict(self,MP,IP,Dist):
         try:
             self.MidToIPDict[MP].append((IP,Dist))
         except KeyError:
             self.MidToIPDict.update({MP:[(IP,Dist)]})
     
-    def UpdateIPToMidDict(self,IP,MP, Dist):
+    def UpdateIPToMidDict(self,IP,MP,Dist):
         try:
             self.IPToMidDict[IP].append((MP,Dist))
         except KeyError:
             self.IPToMidDict.update({IP:[(MP,Dist)]})
 
-    def UpdateAll(self):   
-        usedDict = {} # To stop IPToMidDict from using same midpoint again
+    def UpdateAllDicts(self):   
+        usedDict = {} # Stops IPToMidDict (IP keys) from using same midpoint again
         for NewIP,ListMP in self.IPToMidDict.items():
-            if len(ListMP)==1: # When an IP is only touched by one MP
+            if len(ListMP)==1: # CASE: when an IP is only touched by one MP
+                MP = ListMP[0][0]
                 try:
-                    usedDict[ListMP[0][0]]  # Checks if midpoint is in used dictionary
-                    continue
+                    usedDict[MP]  # Checks if midpoint is in used dictionary
                 except:
-                    usedDict.update({ListMP[0][0]:None})
-                ListIP = self.MidToIPDict[ListMP[0][0]]
+                    usedDict.update({MP:None})
+                else:
+                    continue
+                ListIP = self.MidToIPDict[MP]
                 count = len(ListIP)
-                if count == 1:
-                    self.UpdateDicts(ListMP[0][0],ListIP[0][0])
-                    self.ConnectNewIPs(ListMP[0][0],ListIP[0][0])
-                else: #TODO Use tree to insert IP instead of creating IP angle list
-                    tree = None
-                    self.angleList = []
+                tempOPsList = self.convexHull.midpointDict.pop(MP)
+                leftOfMid = tempOPsList[0]
+                rightOfMid = tempOPsList[1]
+                if count == 1: # CASE: when the MP touches one IP
+                    self.DeleteFromDicts(MP,ListIP[0][0])
+                    self.ConnectNewIPs(ListIP[0][0],leftOfMid,rightOfMid)
+                else: # CASE: when the MP touches multiple IP
+                    self.tree = None
                     for i in range(count):
                         IP = ListIP[i]
-                        leftRightOP = self.convexHull.midpointDict[ListMP[0][0]]
-                        angle = self.GetAngle(leftRightOP[0],leftRightOP[1],IP[0])
-                        tree = self.AngleBinaryTree((IP[0],IP[1],angle),i,tree)
-                        self.CreateAngleListFromTree(tree)
-                        print(self.angleList)
-                        self.UpdateDicts(ListMP[0][0],IP[0])
-                        self.ConnectNewIPs(ListMP[0][0],IP[0],i,count)
-            else: # When multiple Midpoints touch the same IP
+                        angle = self.GetAngle(leftOfMid,rightOfMid,IP[0])
+                        self.InsertIntoOPs((IP[0],angle),leftOfMid,rightOfMid,i,self.tree)
+                        self.DeleteFromDicts(MP,IP[0])
+            else: # CASE: When multiple Midpoints touch the same IP
                 print("multi-case requiring recursion")
                 exit()
 
         print("IP:",len(self.convexHull.IP),"\n",self.convexHull.IP,"\n"*2)
-
-    def AngleBinaryTree(self,angleIP,i,tree=None):
-        if i == 0:
-            self.point = angleIP
-            self.left = None
-            self.right = None
-        elif tree.point[2] > angleIP[2]:
-            if tree.left is not None:
-                tree.left = self.AngleBinaryTree(angleIP,i,tree.left)
-            else:
-                tree.left == self.AngleBinaryTree(angleIP,0)
-        elif tree.point[2] < angleIP[2]:
-            if tree.right is not None:
-                tree.right = self.AngleBinaryTree(angleIP,i,tree.right)
-            else:
-                tree.right == self.AngleBinaryTree(angleIP,0)
     
-    def CreateAngleListFromTree(self,tree):
-        if tree:
-            self.CreateAngleListFromTree(tree.left)
-            self.angleList.append(tree.point)
-            self.CreateAngleListFromTree(tree.right)
+    def InsertIntoOPs(self,angleIP,leftOP,rightOP,i,tree=None):
+        if i == 0:
+            tree.point = angleIP
+            tree.left = None
+            tree.right = None
+            self.ConnectNewIPs(angleIP[0],leftOP,rightOP)
+        elif tree.point[1] > angleIP[1]:
+            if tree.left is not None:
+                tree.left = self.AngleBinaryTree(angleIP,leftOP,tree.point,i,tree.left)
+            else:
+                tree.left == self.AngleBinaryTree(angleIP,leftOP,tree.point,0,tree.left)
+        elif tree.point[1] < angleIP[1]:
+            if tree.right is not None:
+                tree.right = self.AngleBinaryTree(angleIP,tree.point,rightOP,i,tree.right)
+            else:
+                tree.right == self.AngleBinaryTree(angleIP,tree.point,rightOP,0,tree.right)
             
-    def UpdateDicts(self,MP,IP):
+    def DeleteFromDicts(self,MP,IP):
         for p in self.convexHull.midpointDict.keys():
             if p == MP: # Is completely deleted directly after loop
                 continue
@@ -166,28 +167,17 @@ class TravelingSalesmanMidpointAlgo:
             del self.midpointsToIPsRef[p][IP] # Deletes new IP from midpointsToIPsRef
         del self.midpointsToIPs[MP] # Deletes entire midpoint case
 
-    def ConnectNewIPs(self,k,v,i=None,count=None):
-        # if i < count:
-        #     tempOPsList = self.convexHull.midpointDict[k]
-        # elif i == count:
-        tempOPsList = self.convexHull.midpointDict.pop(k)
-        
-        leftOfMid = tempOPsList[0]
-        rightOfMid = tempOPsList[1]
-
-        newNode = AddNode(v,leftOfMid,rightOfMid,self.convexHull.midpointDict)
+    def ConnectNewIPs(self,IP,leftOfMid,rightOfMid):
+        newNode = AddNode(IP,leftOfMid,rightOfMid,self.convexHull.midpointDict)
 
         # update linkedOP
-        self.convexHull.linkedOP[leftOfMid].right = v
+        self.convexHull.linkedOP[leftOfMid].right = IP
         self.convexHull.linkedOP[leftOfMid].rightMidpoint = newNode.leftMidpoint
-        self.convexHull.linkedOP[rightOfMid].left = v
+        self.convexHull.linkedOP[rightOfMid].left = IP
         self.convexHull.linkedOP[rightOfMid].leftMidpoint = newNode.rightMidpoint
-        self.convexHull.linkedOP.update({v : newNode})
+        self.convexHull.linkedOP.update({IP : newNode})
 
         print("linkedOP:",self.convexHull.linkedOP,"\n"*2)
-
-        self.UpdateOP(v)
-        self.UpdateIP(v)
 
         # update midpointsToIPs
         print("New midpointsToIPs:","\n")
@@ -195,11 +185,10 @@ class TravelingSalesmanMidpointAlgo:
         self.AddToMidpointToIPs(newNode,"left")
         print("\n")
         
-    def UpdateOP(self,IP):
-        self.convexHull.OP.append(IP) 
-
-    def UpdateIP(self,IP):
-        self.convexHull.IP.remove(IP) 
+    def Update_OP_IP(self):
+        for IP in self.SetIP:
+            self.convexHull.OP.append(IP) 
+            self.convexHull.IP.remove(IP) 
     
     def PrintConnectedOP(self):
         printList = []
