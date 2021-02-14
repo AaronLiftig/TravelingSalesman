5f3affd3ad34951dc0f18a1462d2dc21add0f85f
Aaron Liftig <49658070+AaronL87@users.noreply.github.com>
2021-02-14 02:34:12 -0500

    Update convex_hull.py

--- convex_hull.py
+++ convex_hull.py
@@ -5,25 +5,31 @@ from link_nodes import LinkNodes
 
 class CreateConvexHull:
     def __init__(self,number_of_points,range_of_points,point_list):
-        if point_list is None and number_of_points is not None and range_of_points is not None:
+        if point_list is None and number_of_points is not None \
+        and range_of_points is not None:
             try:
                 number_of_points = int(number_of_points)
                 range_of_points = int(range_of_points)
                 if number_of_points < 1 or range_of_points < 1:
-                    print("Both number_of_points and range_of_points must be positive integers,")
+                    print("Both number_of_points and range_of_points" 
+                        + " must be positive integers,")
                     exit()
             except:
-                print("Both number_of_points and range_of_points must be positive integers,"
-                " or enter number_of_points as a list of (x,y) points with integer values")
+                print("Both number_of_points and range_of_points must" 
+                    + " be positive integers, or enter number_of_points" 
+                    + " as a list of (x,y) points with integer values")
                 exit()
             print("number_of_points:",number_of_points)
             print("range_of_points:",range_of_points,"\n"*2)          
-            all_points = self.get_random_points(number_of_points,range_of_points)         
-        elif isinstance(point_list,list) and number_of_points is None and range_of_points is None \
+            all_points = self.get_random_points(number_of_points,
+                                                range_of_points)         
+        elif isinstance(point_list,list) and number_of_points is None \
+        and range_of_points is None \
         and all(list(map(type,tup)) == [int,int] for tup in point_list):  
             all_points = list(set(point_list))   
         else:
-            print("Either point_list must be a list of integers or number_of_points and range_of_points must be positive integers")
+            print("Either point_list must be a list of integers or number_of_points" 
+                + " and range_of_points must be positive integers")
             exit()
         self.separate_points(all_points)
         self.create_linked_list()
@@ -32,12 +38,15 @@ class CreateConvexHull:
         all_points = [] 
 
         for i in range(number_of_points):
-            points = (float(np.random.randint(-range_of_points,range_of_points)),
-                        float(np.random.randint(-range_of_points,range_of_points)))
+            points = (float(np.random.randint(-range_of_points,
+                                                range_of_points)),
+                        float(np.random.randint(-range_of_points,
+                                                range_of_points)))
             all_points.append(points)
         # creates list of random points
 
-        all_points = list(set(all_points)) #takes out possible duplicates
+        all_points = list(set(all_points)) 
+        #takes out possible duplicates
         return all_points
 
     def separate_points(self,all_points):    
@@ -47,12 +56,14 @@ class CreateConvexHull:
         print("OP:",self.OP,"\n") # Prints points of convex hull (OP)
 
         self.IP = [x for x in all_points if x not in self.OP]
-        print("IP:",len(self.IP),"\n",self.IP,"\n"*2) # Prints all inner points (not in OP)
+        print("IP:",len(self.IP),"\n",self.IP,"\n"*2) 
+        # Prints all inner points (not in OP)
 
     def create_linked_list(self):
         self.linked_OP = {}
         self.MP_dictionary = {} # For referencing efficency
         for i in range(len(self.OP)):
-            self.linked_OP.update({self.OP[i] : LinkNodes(self.OP,i,self.MP_dictionary)})
+            self.linked_OP.update({self.OP[i] : LinkNodes(self.OP,i,
+                                                        self.MP_dictionary)})
         
         print("MP_dictionary:",self.MP_dictionary,"\n"*2)           
